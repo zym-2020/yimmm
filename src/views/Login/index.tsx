c082cd6b0a1e3787126cf4036ecc8a09c799d8ec
yimmm <1161384816@qq.com>
2025-01-08 23:46:37 +0800

    add loginPage

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,25 +1,73 @@
-import { Button } from "antd";
+import { useState } from "react";
+import { Button, Form, Input, Flex } from "antd";
 import { useNavigate } from "react-router";
 import { login } from "@/request";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import "./index.less";
 
+const LoginForm = () => {
+  const navigate = useNavigate();
+  const [form] = Form.useForm();
+  const [account, setAccount] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginFn = () => {
+    form
+      .validateFields()
+      .then(async () => {
+        const res = await login(account, password);
+        if (res) {
+          localStorage.setItem("token", res.data);
+          navigate("/");
+        }
+      })
+      .catch(() => {});
+  };
+  return (
+    <div className="login-form">
+      <Form form={form}>
+        <Form.Item
+          name="account"
+          rules={[{ required: true, message: "请输入账号!" }]}>
+          <Input
+            prefix={<UserOutlined />}
+            placeholder="账号"
+            onChange={(e) => {
+              setAccount(e.target.value);
+            }}
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "请输入密码!" }]}>
+          <Input
+            prefix={<LockOutlined />}
+            placeholder="密码"
+            type="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button block type="primary" onClick={loginFn}>
+            登录
+          </Button>
+        </Form.Item>
+      </Form>
+      <Flex justify="space-between" align="center">
+        <span className="login-form-text-btn">注册</span>
+        <span className="login-form-text-btn">忘记密码？</span>
+      </Flex>
+    </div>
+  );
+};
 
 const Login = () => {
-  const navigate = useNavigate();
-  const loginFn = async () => {
-    const res = await login("1161384816@qq.com", "123");
-    if (res) {
-      localStorage.setItem("token", res.data);
-      navigate("/");
-    }
-  };
-  const clickHandle = () => {
-    loginFn()
-  }
   return (
-    <>
-      <div>登录页</div>
-      <Button onClick={clickHandle}>登录</Button>
-    </>
+    <div className="login-page">
+      <LoginForm />
+    </div>
   );
 };
 
